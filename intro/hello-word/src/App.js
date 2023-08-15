5714cdc776ca1506b1dc01a06a996c42dae1ee87
naiaragabriela <naigabi31@gmail.com>
2023-08-15 18:47:27 -0300

    feat: commit inicial

---
+++ intro/hello-word/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Word React</h1>
+     <h2> Emment funcionou</h2>
+     <p> Meu primeiro parágrafo em React</p>
+    </div>
+  );
+}
+
+export default App;
