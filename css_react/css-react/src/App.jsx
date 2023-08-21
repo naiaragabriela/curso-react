0b54d8416ad2a991279e368559d0aa9df87bf02a
naiaragabriela <naigabi31@gmail.com>
2023-08-21 16:48:07 -0300

    feat: react CSS

---
+++ css_react/css-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Mycomponent from './components/Mycomponent'
+
+function App() {
+  return (
+    <>
+    {/*CSS GLOBAL */}
+    <h1>React com CSS</h1>
+     {/*CSS Componente */}
+    <Mycomponent />
+    </>
+  )
+}
+
+export default App
---
+++ css_react/css-react/src/components/Mycomponent.jsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const Mycomponent = () => {
+  return (
+    <div>
+        <h1>CSS de Componente</h1>
+        <p> Paragráfo do Componente</p>
+        <p className="my-comp-paragraph"> Este também é do Componente</p>
+    </div>
+  )
+}
+
+export default Mycomponent
