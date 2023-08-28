590fa17fe2994cef23e23c1c5344d496090f3210
naiaragabriela <naigabi31@gmail.com>
2023-08-28 09:17:33 -0300

    feat: react router

---
+++ react_router/reactrouter/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+//1- config react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//2- pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>React Router</h1>
+     <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route paht="/about" element={<About />} />
+       </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
