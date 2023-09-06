cbe6b39fd2a63097e8141bd3ac88d0b8bd2ec384
naiaragabriela <naigabi31@gmail.com>
2023-09-06 12:31:17 -0300

    feat: navigation between pages

--- react_router/reactrouter/src/App.js
+++ react_router/reactrouter/src/App.js
@@ -7,15 +7,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
+//3-components
+import Navibar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
      <h1>React Router</h1>
      <BrowserRouter>
-       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route paht="/about" element={<About />} />
-       </Routes>
+      <Navibar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
      </BrowserRouter>
     </div>
   );
