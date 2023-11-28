7a7e945932278eaf842901602dd708a75a626e69
naiaragabriela <naigabi31@gmail.com>
2023-11-28 20:39:38 -0300

    refactor: the method post

--- 7_req_http/httpreact/src/App.js
+++ 7_req_http/httpreact/src/App.js
@@ -2,22 +2,29 @@ import './App.css';
 
 import { useState, useEffect } from 'react';
 
+//4-Custom hook
+import { useFetch } from './hooks/useFetch';
+
 const url = "http://localhost:3000/product/";
 
 function App() {
   const [product, setProduct] = useState([]);
+//4 - Custom hook
+  const { data: items, httpConfig } = useFetch(url);
+
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   //1- resgatando dados (chamada assincrona)
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(url);
-      const data = await response.json();
-      setProduct(data);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     setProduct(data);
+  //   };
+  //   fetchData();
+  // }, []);
   
   //2- Adição de produtos
   const handleSubmit = async (e) => {
@@ -27,18 +34,22 @@ function App() {
       price
     };
     
-    //criando a requisição
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product)
-    });
+    // //criando a requisição
+    //const res = await fetch(url, {
+     //method: "POST",
+      //headers: {
+        //"Content-Type": "application/json"
+      //},
+      //body: JSON.stringify(product)
+    //});
 
-    const addedProduct = await res.json();
-  //3-carregamento dinâmico
-  setProduct((prevProducts) => [...prevProducts, addedProduct]);
+    //const addedProduct = await res.json();
+  // //3-carregamento dinâmico
+  //setProduct((prevProducts) => [...prevProducts, addedProduct]);
+
+  // 5- refatorando o Post
+  httpConfig(product, "POST");
+
   setName("");
   setPrice("");
    
@@ -48,7 +59,7 @@ function App() {
     <div className="App">
       <h1>Lista de produtos</h1>
       <ul>
-        {product.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>
             {product.name} - R$: {product.price}
           </li>
