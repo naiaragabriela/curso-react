3359f8077c372a4c562723c5009b7178379948b0
naiaragabriela <naigabi31@gmail.com>
2023-11-28 17:09:19 -0300

    fix: Navbar component corrected name

--- 8_react_router/reactrouter/src/App.js
+++ 8_react_router/reactrouter/src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 
 //1- config react router
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 //2- pages
 import Home from './pages/Home';
 import About from './pages/About';
 
 //3-components
-import Navibar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
      <h1>React Router</h1>
      <BrowserRouter>
-      <Navibar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
