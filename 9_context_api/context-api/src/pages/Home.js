a15a092b4a59c16e418e372d01f585dcfda30f63
naiaragabriela <naigabi31@gmail.com>
2024-09-09 21:53:51 -0300

    Feat: Create a complex context

--- 9_context_api/context-api/src/pages/Home.js
+++ 9_context_api/context-api/src/pages/Home.js
@@ -1,17 +1,24 @@
 // import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
-// import ChangeCounter from "../components/ChangeCounter";
+// import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
 
 //4- Refatorando com hook
 import { useCounterContext } from "../hooks/useCounterContext";
+//5- Context mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
 
 const Home = () => {
     // const { counter } = useContext(CounterContext);
     const { counter } = useCounterContext();
     
+    //5- Context mais complexo
+    const { color } = useTitleColorContext();
+
+
     return(
         <div>
-            <h1>Home</h1>
+            <h1 style={{ color: color }}>Home</h1>
             <p>Valor do contador: {counter}</p>
             {/* 3- alterando o valor do contexto*/}
             <ChangeCounter />
