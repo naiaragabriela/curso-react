726401a9feb1375413072b1805c0b6d34175334c
naiaragabriela <naigabi31@gmail.com>
2024-09-09 22:10:17 -0300

    Feat: Change complex context

--- 9_context_api/context-api/src/pages/About.js
+++ 9_context_api/context-api/src/pages/About.js
@@ -2,15 +2,20 @@ import { useContext } from "react";
 import { CounterContext } from "../context/CounterContext";
 //4- Refatorando com hook
 import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 
 const About = () => {
     // const { counter } = useContext(CounterContext);
     const { counter } = useCounterContext();
 
+      //5- Context mais complexo
+      const { color } = useTitleColorContext();
+
+
     return(
         <div>
-            <h1>About</h1>
+            <h1 style={{ color: color }}>About</h1>
             <p>Valor do contador: {counter}</p>
         </div>
     )
--- 9_context_api/context-api/src/pages/Home.js
+++ 9_context_api/context-api/src/pages/Home.js
@@ -13,7 +13,12 @@ const Home = () => {
     const { counter } = useCounterContext();
     
     //5- Context mais complexo
-    const { color } = useTitleColorContext();
+    const { color, dispatch } = useTitleColorContext();
+
+    //6- Alterando um state mais complexo
+    const setTitleColor = (color) => {
+        dispatch({type: color});
+    };
 
 
     return(
@@ -22,6 +27,11 @@ const Home = () => {
             <p>Valor do contador: {counter}</p>
             {/* 3- alterando o valor do contexto*/}
             <ChangeCounter />
+            {/*6- alterando contexto complexo */}
+            <div>
+                <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+                <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+            </div>
         </div>
     )
 };
