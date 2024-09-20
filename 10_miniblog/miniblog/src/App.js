b330dd7689e58fba9ca7a9e0ca5b4b107ab60a68
naiaragabriela <naigabi31@gmail.com>
2024-09-19 22:13:45 -0300

    Feat: Add navbar and footer

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
 // pages 
@@ -10,12 +12,14 @@ function App() {
   return (
     <div className="App">
      <BrowserRouter>
+     <NavBar />
      <div className='container'>
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
      </Routes>
      </div>
+     <Footer />
      </BrowserRouter>
     </div>
   );
